4f54ff5bec7d2a6428d5e0acdbe9a54002e374d4
DESKTOP-GE1GSCB\aguss <aguss_979@hotmail.com>
2021-08-17 14:48:32 -0300

    .

---
+++ src/main/java/com/mindhub/Lollapalooza/configurations/WebAuthentication.java
@@ -0,0 +1,55 @@
+package com.mindhub.Lollapalooza.configurations;
+
+import ch.qos.logback.core.net.server.Client;
+import com.mindhub.Lollapalooza.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(inputName-> {
+
+            Client client = clientRepository.findByEmail(inputName);
+
+            if(client.getEmail().equalsIgnoreCase("mabel@gmail.com")){
+                return new User(client.getEmail(), client.getPassword(),
+                        AuthorityUtils.createAuthorityList("ADMIN"));
+
+            }
+            if (client != null) {
+
+                return new User(client.getEmail(), client.getPassword(),
+
+                        AuthorityUtils.createAuthorityList("USER"));
+
+            } else {
+
+                throw new UsernameNotFoundException("Unknown user: " + inputName);
+
+            }
+
+        });
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    }
+
+}
