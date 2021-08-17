31bdeeac496aa1fa1eaf89734e7593cd2e7e2248
DESKTOP-GE1GSCB\aguss <aguss_979@hotmail.com>
2021-08-17 15:27:06 -0300

    add fix

--- src/main/java/com/mindhub/Lollapalooza/configurations/WebAuthentication.java
+++ src/main/java/com/mindhub/Lollapalooza/configurations/WebAuthentication.java
@@ -5,6 +5,7 @@ import com.mindhub.Lollapalooza.models.Client;
 import com.mindhub.Lollapalooza.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -23,7 +24,7 @@ public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
 
         auth.userDetailsService(inputName-> {
 
-            Client client = clientRepository.findByEmail(inputName);
+            Client client = clientRepository.findByUser(inputName);
 
             if(client.getUser().equalsIgnoreCase("user@gmail.com")){
                 return new User(client.getUser(), client.getPassword(),
