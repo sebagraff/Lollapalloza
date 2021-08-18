14602cf360c82f6bfc414e2acdb49a055ea42dfb
Juan Diego <jdiegoferretto@hotmail.com>
2021-08-18 16:11:03 -0300

    .

--- src/main/resources/static/Web/js/login.js
+++ src/main/resources/static/Web/js/login.js
@@ -4,6 +4,45 @@ let form_register = document.querySelector(".form__register")
 let container_login_register = document.querySelector(".container__login-register")
 let back_box_login = document.querySelector(".back__box-login")
 let back_box_register = document.querySelector(".back__box-register")
+let back__box = document.querySelector(".back__box")
+
+window.addEventListener("resize", widthPage);
+
+function widthPage(){
+    if(window.innerWidth > 850){
+        back_box_login.style.display = "block";
+        back_box_register.style.display = "block";
+        back__box.style.background = "linear-gradient(0.25turn, #00af9a, #ff4034)"
+    }else{
+        back_box_register.style.display = "block";
+        back_box_register.style.opacity = "1";
+        back_box_login.style.display = "none";
+        form_login.style.display = "block";
+        form_register.style.display = "none";
+        container_login_register.style.left = "0";
+        back__box.style.background = "rgba(87, 88, 90, 1)";
+    }
+}
+
+widthPage();
+
+/* Cuando se clickea, cambia de lugar */
+const singIn = () => {
+    
+    if (window.innerWidth > 850) {
+        form_register.style.display = "none";
+        container_login_register.style.left = "10px";
+        form_login.style.display = "block";
+        back_box_register.style.opacity = "1";
+        back_box_login.style.opacity = "0";
+    } else {
+        form_register.style.display = "none";
+        container_login_register.style.left = "0px";
+        form_login.style.display = "block";
+        back_box_register.style.display = "block";
+        back_box_login.style.display = "none";
+    }
+}
 
 /* Cuando se clickea cambia de lugar */
 const register = () => {
@@ -20,24 +59,5 @@ const register = () => {
         back_box_register.style.display = "none";
         back_box_login.style.display = "block";
         back_box_login.style.opacity = "1";
-    }
-}
-
-
-/* Cuando se clickea, cambia de lugar */
-const singIn = () => {
-
-    if (window.innerWidth > 850) {
-        form_register.style.display = "none";
-        container_login_register.style.left = "10px";
-        form_login.style.display = "block";
-        back_box_register.style.opacity = "1";
-        back_box_login.style.opacity = "0";
-    } else {
-        form_register.style.display = "none";
-        container_login_register.style.left = "0px";
-        form_login.style.display = "block";
-        back_box_register.style.display = "block";
-        back_box_login.style.display = "none";
     }
 }
