3209e32a2765387976b0f446ef5aba3ce932d7de
Emmanuel Leiva <ema62x@gmail.com>
2021-08-20 03:25:17 -0300

    .

--- src/main/resources/static/Web/js/merchandising.js
+++ src/main/resources/static/Web/js/merchandising.js
@@ -3,28 +3,30 @@ const app = Vue.createApp({
         return {
             products: {},
             AuxProducts: {},
-            inputSearch:"",
-            cart:[]
+            inputSearch: "",
+            cart: []
         }
     },
 
     created() {
         axios.get("http://localhost:8080/api/products")
-        .then(res =>{
-            this.products = res.data
-            this.AuxProducts = this.products;
-        })
+            .then(res => {
+                this.products = res.data
+                console.log(this.products)
+                this.AuxProducts = this.products.sort((a, b) => b.price - a.price);
+
+            })
     },
 
     methods: {
-        handlePushCarrito(product){
+        handlePushCarrito(product) {
             this.cart.push(product)
             console.log(this.cart)
             product.stock--
         }
     },
     computed: {
-        filterProducts(){
+        filterProducts() {
             this.AuxProducts = this.products.filter(product => product.name.toUpperCase().indexOf(this.inputSearch.toUpperCase()) !== -1)
         }
     }
