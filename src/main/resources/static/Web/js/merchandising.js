b335788e8af84dc53df49bf5c7306d7a8ecc1fb6
DESKTOP-GE1GSCB\aguss <aguss_979@hotmail.com>
2021-08-19 22:59:04 -0300

    add merch & jqerys

---
+++ src/main/resources/static/Web/js/merchandising.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            products: {},
+            AuxProducts: {},
+            inputSearch:"",
+            cart:[]
+        }
+    },
+
+    created() {
+        axios.get("http://localhost:8080/api/products")
+        .then(res =>{
+            this.products = res.data
+            this.AuxProducts = this.products;
+        })
+    },
+
+    methods: {
+        handlePushCarrito(product){
+            this.cart.push(product)
+            console.log(this.cart)
+            product.stock--
+        }
+    },
+    computed: {
+        filterProducts(){
+            this.AuxProducts = this.products.filter(product => product.name.toUpperCase().indexOf(this.inputSearch.toUpperCase()) !== -1)
+        }
+    }
+
+})
+app.mount("#app")
