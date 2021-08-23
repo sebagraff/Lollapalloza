3e641c6dd6b6450b16fe22e9958f9ee1001d82c6
sebagraff <sebastiandavidgraff@gmail.com>
2021-08-23 10:18:09 -0300

    Agregado, gallery beta 1, bands y bands copy beta. creado MusicBandDTO EventDTO

--- src/main/java/com/mindhub/Lollapalooza/controllers/MusicBandController.java
+++ src/main/java/com/mindhub/Lollapalooza/controllers/MusicBandController.java
@@ -1,9 +1,16 @@
 package com.mindhub.Lollapalooza.controllers;
 
+import com.mindhub.Lollapalooza.dtos.MusicBandDTO;
+import com.mindhub.Lollapalooza.dtos.ProductDTO;
+import com.mindhub.Lollapalooza.repositories.EventRepository;
 import com.mindhub.Lollapalooza.repositories.MusicBandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -11,4 +18,14 @@ public class MusicBandController {
 
     @Autowired
     MusicBandRepository musicBandRepository;
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @GetMapping("/musicbands")
+    public List<MusicBandDTO> getAll(){
+        return this.musicBandRepository.findAll().stream().map(MusicBandDTO::new).collect(Collectors.toList());
+    }
+
+
 }
---
+++ src/main/resources/static/Web/js/bands.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            musicbands: {},
+            cart: [],
+
+        }
+    },
+
+    created() {
+        axios.get("http://localhost:8080/api/musicbands")
+        .then(res => {
+            this.musicbands = res.data
+        })
+
+    },
+
+    methods: {
+        ticketsLoginButton(){
+            window.location.href="login.html"
+        }
+    },
+
+    computed: {
+        
+
+    }
+
+})
+
+app.mount("#app")
+
+
+let myCarousel = document.querySelector('#musicBandCarousel')
+let carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 5000,
+    wrap: true
+})
