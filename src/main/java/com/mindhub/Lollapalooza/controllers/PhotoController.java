6cd5c05fbd5695506aed96a7c358a434ee5c706b
sebagraff <sebastiandavidgraff@gmail.com>
2021-08-26 16:07:51 -0300

    photo/comment

--- src/main/java/com/mindhub/Lollapalooza/controllers/PhotoController.java
+++ src/main/java/com/mindhub/Lollapalooza/controllers/PhotoController.java
@@ -2,19 +2,22 @@ package com.mindhub.Lollapalooza.controllers;
 
 import com.mindhub.Lollapalooza.dtos.MusicBandDTO;
 import com.mindhub.Lollapalooza.dtos.PhotoDTO;
+import com.mindhub.Lollapalooza.dtos.ProductDTO;
+import com.mindhub.Lollapalooza.models.Photo;
+import com.mindhub.Lollapalooza.models.Product;
 import com.mindhub.Lollapalooza.repositories.CommentsRepository;
 import com.mindhub.Lollapalooza.repositories.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class PhotoController {
 
     @Autowired
@@ -33,6 +36,21 @@ public class PhotoController {
         return this.photoRepository.findById(id).map(PhotoDTO::new).orElse(null);
     }
 
-    CommentController commentController;
+    @DeleteMapping("/photos/{id}")
+    public ResponseEntity<?> deletePhoto(@PathVariable Long id){
+        Photo photo = photoRepository.findById(id).get();
+        photoRepository.delete(photo);
+        return new ResponseEntity<>("Eliminado con éxito", HttpStatus.OK);
+    }
+
+    // Hay que hacer verificaciones con el usuario logueado
+
+    @PostMapping("/photos")
+    public ResponseEntity<?> addPhoto(@RequestBody PhotoDTO photoDTO){
+
+        this.photoRepository.save(new Photo(photoDTO.getImgTitle(), photoDTO.getImgUrl(), photoDTO.getDescription()));
+        return new ResponseEntity<>("Producto agregado correctamente", HttpStatus.ACCEPTED);
+
+    }
 
 }
--- src/main/java/com/mindhub/Lollapalooza/repositories/CommentsRepository.java
+++ src/main/java/com/mindhub/Lollapalooza/repositories/CommentsRepository.java
@@ -4,8 +4,12 @@ import com.mindhub.Lollapalooza.models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CommentsRepository extends JpaRepository<Comment, Long> {
     Comment findById (long id);
+    List<Comment> findByPhotoId(long id);
+
 
 }
