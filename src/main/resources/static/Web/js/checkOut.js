515d652932330d113be4a32533783eb4610b43fb
Uko Benelli <uko@Ukos.Mint>
2021-08-26 17:01:02 -0300

    update del checkOut

--- src/main/resources/static/Web/js/checkOut.js
+++ src/main/resources/static/Web/js/checkOut.js
@@ -1,23 +1,34 @@
 const app = Vue.createApp({
     data() {
         return {
-
+            currentClient:0,
+            carrito=[],
         }
     },
 
     created() {
-        axios.get("api/cart")
-        .then(res=>{
-            console.log(res)
-        })
+        axios.get("/api/clients/current")
+            .then(res => {
+                this.currentClient = res.data
+                console.log(this.currentClient.id)
+
+            }).then(() => {
+                axios.get("/api/cart/" + this.currentClient.id)
+                    .then(res => {
+                        console.log(res.data.productsInCart)
+                        this.carrito=res.data.productsInCart;
+                    })
+            })
     },
 
-    methods: {
+    methods: {/*
+        subTotal(){
+            this.
+        }*/
     },
 
     computed: {
-
-    }
+    },
 
 })
 
